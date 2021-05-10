4f9184ccb8046afa50867b8b55193ef4188549bf
Insaf Ismagilov <insaf.ismg@gmail.com>
2021-05-10 19:04:31 +0300

    Added input values validation.

--- SignatureCreator/Program.cs
+++ SignatureCreator/Program.cs
@@ -6,27 +6,47 @@ using SignatureCreator.Options;
 
 namespace SignatureCreator
 {
-	class Program
+	internal static class Program
 	{
-		static void Main(string[] args)
+		public static void Main(string[] args)
 		{
-			// TODO: inputs validation.
-			var fileName = args[0];
-			var blockSize = long.Parse(args[1]);
+			try
+			{
+				var options = GetOptions(args);
 
-			var signatureCreator = new FileSignatureCreator(new SignatureCreatorOptions
+				var signatureCreator = new FileSignatureCreator(options);
+
+				var sw = Stopwatch.StartNew();
+				var signature = signatureCreator.ComputeFileSignature();
+				sw.Stop();
+
+				PrintSignature(signature);
+				Console.WriteLine("-----------------------------------------");
+				Console.WriteLine($"Execution time: {sw.ElapsedMilliseconds} ms");
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e);
+			}
+		}
+
+		private static SignatureCreatorOptions GetOptions(string[] args)
+		{
+			if (args == null || args.Length < 2)
+				throw new ArgumentException(nameof(args));
+
+			string fileName = !string.IsNullOrEmpty(args[0])
+				? args[0]
+				: throw new ArgumentException(nameof(fileName));
+
+			if (!long.TryParse(args[1], out var blockSize))
+				throw new ArgumentException(nameof(blockSize));
+
+			return new SignatureCreatorOptions
 			{
 				FileName = fileName,
 				BlockSize = blockSize
-			});
-
-			var sw = Stopwatch.StartNew();
-			var signature = signatureCreator.ComputeFileSignature();
-			sw.Stop();
-			
-			PrintSignature(signature);
-			Console.WriteLine("-----------------------------------------");
-			Console.WriteLine($"Execution time: {sw.ElapsedMilliseconds} ms");
+			};
 		}
 
 		private static void PrintSignature(IReadOnlyDictionary<int, string> signature)
