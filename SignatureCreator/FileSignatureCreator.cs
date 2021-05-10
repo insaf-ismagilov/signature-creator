fd36a3fb282e38e743b78d5f1f725f4b16072035
Insaf Ismagilov <insaf.ismg@gmail.com>
2021-05-10 13:39:41 +0300

    Implement file signing.

---
+++ SignatureCreator/FileSignatureCreator.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace SignatureCreator
+{
+	public class FileSignatureCreator
+	{
+		private readonly string _fileName;
+		private readonly long _blockSize;
+		
+		public FileSignatureCreator(string fileName, long blockSize)
+		{
+			_fileName = fileName;
+			_blockSize = blockSize;
+		}
+		
+		public IReadOnlyDictionary<int, string> ComputeFileSignature()
+		{
+			// TODO: change to multi-thread approach.
+			var result = new ConcurrentDictionary<int, string>();
+
+			long offset = 0;
+			var blockEnd = _blockSize;
+			var fileSize = GetFileSize();
+
+			var blocksCount = (int) Math.Ceiling((double) fileSize / _blockSize);
+
+			for (int i = 0; i < blocksCount; ++i)
+			{
+				var blockHash = ComputeBlockHash(offset, blockEnd);
+				result.TryAdd(i, blockHash);
+
+				offset += _blockSize;
+				blockEnd += _blockSize;
+			}
+
+			return result;
+		}
+
+		private long GetFileSize()
+		{
+			using var fs = new FileStream(_fileName, FileMode.Open, FileAccess.Read, FileShare.Read);
+			return fs.Length;
+		}
+
+		private string ComputeBlockHash(long offset, long blockEnd)
+		{
+			var buffer = new byte[_blockSize];
+			using var sha256 = new SHA256Managed();
+			using var fs = new FileStream(_fileName, FileMode.Open, FileAccess.Read, FileShare.Read);
+				
+			while (true)
+			{
+				fs.Seek(offset, SeekOrigin.Begin);
+				var bytesRead = fs.Read(buffer, 0, buffer.Length);
+
+				offset += buffer.Length;
+
+				if (blockEnd > offset)
+					sha256.TransformBlock(buffer, 0, buffer.Length, buffer, 0);
+				else
+				{
+					sha256.TransformFinalBlock(buffer, 0, bytesRead);
+					return ConvertBytesToString(sha256.Hash);
+				}
+			}
+		}
+		
+		private static string ConvertBytesToString(byte[] hashBytes)
+		{
+			var stringBuilder = new StringBuilder();
+
+			foreach (var b in hashBytes)
+				stringBuilder.AppendFormat("{0:X2}", b);
+
+			return stringBuilder.ToString();
+		}
+	}
+}
--- SignatureCreator/Program.cs
+++ SignatureCreator/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Security.Cryptography;
-using System.Text;
+using System.Linq;
 
 namespace SignatureCreator
 {
@@ -10,65 +8,23 @@ namespace SignatureCreator
 	{
 		static void Main(string[] args)
 		{
-			var filePath = args[0];
+			// TODO: inputs validation.
+			var fileName = args[0];
 			var blockSize = long.Parse(args[1]);
 
-			var hashes = ComputeBlockHashes(filePath, blockSize);
+			var signatureCreator = new FileSignatureCreator(fileName, blockSize);
 
-			foreach (var (blockNumber, hash) in hashes)
+			var signature = signatureCreator.ComputeFileSignature();
+
+			PrintSignature(signature);
+		}
+
+		private static void PrintSignature(IReadOnlyDictionary<int, string> signature)
+		{
+			foreach (var (blockNumber, hash) in signature.OrderBy(x => x.Key))
 			{
 				Console.WriteLine($"{blockNumber}: {hash}");
 			}
-		}
-		
-		private static string ConvertBytesToString(byte[] hashBytes)
-		{
-			var stringBuilder = new StringBuilder();
-
-			foreach (var b in hashBytes)
-				stringBuilder.AppendFormat("{0:X2}", b);
-
-			return stringBuilder.ToString();
-		}
-		
-		private static List<(int, string)> ComputeBlockHashes(string filePath, long blockSize)
-		{
-			var result = new List<(int, string)>();
-
-			long offset = 0;
-			long blockEnd = blockSize;
-
-			using var fs = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.Read);
-
-			var blocksCount = (int) Math.Ceiling((double) fs.Length / blockSize);
-
-			for (int i = 0; i < blocksCount; ++i)
-			{
-				var buffer = new byte[blockSize];
-				
-				using var sha256 = new SHA256Managed();
-				
-				while (true)
-				{
-					fs.Seek(offset, SeekOrigin.Begin);
-					var bytesRead = fs.Read(buffer, 0, buffer.Length);
-
-					offset += buffer.Length;
-
-					if (blockEnd > offset)
-						sha256.TransformBlock(buffer, 0, buffer.Length, buffer, 0);
-					else
-					{
-						sha256.TransformFinalBlock(buffer, 0, bytesRead);
-						result.Add((i, ConvertBytesToString(sha256.Hash)));
-						break;
-					}
-				}
-				
-				blockEnd += blockSize;
-			}
-
-			return result;
 		}
 	}
 }
